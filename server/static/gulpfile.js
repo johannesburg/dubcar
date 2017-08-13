cb0177a008f5eaa00866757ef4080b9f3f6a0670
Johan Michalove <michajoh@cs.uw.edu>
2017-08-13 14:36:25 -0700

    Update gulp to support babelify.

--- server/static/gulpfile.js
+++ server/static/gulpfile.js
@@ -1,27 +1,39 @@
-// requirements
-
 var gulp = require('gulp');
-var gulpBrowser = require("gulp-browser");
-var reactify = require('reactify');
-var del = require('del');
-var size = require('gulp-size');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
 
+function compile(watch) {
+  var bundler = watchify(browserify('./scripts/jsx/main.js', { debug: true }).transform(babel, {presets: ["react"]}));
 
-// tasks
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./scripts/js'));
+  }
 
-gulp.task('transform', function () {
-  var stream = gulp.src('./scripts/jsx/*.js')
-    .pipe(gulpBrowser.browserify({transform: ['reactify']}))
-    .pipe(gulp.dest('./scripts/js/'))
-    .pipe(size());
-  return stream;
-});
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
 
-gulp.task('del', function () {
-  return del(['./scripts/js']);
-});
+  rebundle();
+}
 
-gulp.task('default', ['del'], function () {
-  gulp.start('transform');
-  gulp.watch('./scripts/jsx/*.js', ['transform']);
-});
+function watch() {
+  return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
